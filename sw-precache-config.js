c038b435e48adfc4b9d86cf902b8b7d1be94d154
Bo-Yan Cheng <markxp2002@gmail.com>
2018-05-10 17:37:56 +0800

    comments for clearity

--- sw-precache-config.js
+++ sw-precache-config.js
@@ -1,34 +1,37 @@
 module.exports = {
-    staticFileGlobs: [
-      '/index.html',
-      '/manifest.json',
-      '/bower_components/webcomponentsjs/webcomponents-lite.js'
-    ],
-    navigateFallback: '/index.html',
+  staticFileGlobs: [
+    '/index.html',
+    '/manifest.json',
+   '/bower_components/webcomponentsjs/webcomponents-lite.js'
+  ],
+  navigateFallback: '/index.html',
 
-    //This option allows us to serve the app shell while the app is offline for any client-side route.
-    navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],    
-    runtimeCaching: [
-      // an image for unavailable youtube video
-      {
-        urlPattern: /^https:\/\/i\.ytimg\.com/,
-        handler: 'fastest',
-        options: {
-          cache: {
-            maxEntries: 100,
-            name: 'yt-images-cache'
-          }
-        }
-      },
-      {
-        urlPattern: /^https:\/\/www\.googleapis\.com\/youtube\/v3\/search/,
-        handler: 'networkFirst',
-        options: {
-          cache: {
-            maxEntries: 100,
-            name: 'yt-data-cache'
-          }
+  //This option allows us to serve the app shell while the app is offline for any client-side route.
+  navigateFallbackWhitelist: [/^(?!.*\.html$|\/data\/).*/],    
+
+  
+  // runtime (dynamic) caching
+  runtimeCaching: [
+  // an image for unavailable youtube video
+    {
+      urlPattern: /^https:\/\/i\.ytimg\.com/,
+      handler: 'fastest',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'yt-images-cache'
         }
       }
-    ]
-  };
+    },
+    {
+      urlPattern: /^https:\/\/www\.googleapis\.com\/youtube\/v3\/search/,
+      handler: 'networkFirst',
+      options: {
+        cache: {
+          maxEntries: 100,
+          name: 'yt-data-cache'
+        }
+      }
+    }
+  ]
+};
